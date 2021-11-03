5c33ab5ab5f2d9600d51208352e96af571d16536
NullAlu <marcotm98@hotmail.com>
2021-11-03 12:33:56 +0100

    VistaUsuario
    Primera impresión de la vista del usuario con su controlador vacío.

--- GestionApp/src/main/java/module-info.java
+++ GestionApp/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.gestionapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.gestionapp to javafx.fxml;
     exports com.mycompany.gestionapp;
