7ab32d6d6b06a182cc857620f90b1e51c756fee1
Jesus Ariza <jmoisesariza@gmail.com>
2021-11-09 11:24:54 +0100

    Nuevos controllers y vistas implementados

--- GestionApp/src/main/java/com/mycompany/gestionapp/App.java
+++ GestionApp/src/main/java/com/mycompany/gestionapp/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginVista"), 343, 536);
+        //Para programar vuestra vista y ver resultados teneis que cambiar aqui el nombre
+        scene = new Scene(loadFXML("loginVista"), 343, 536); 
         stage.setScene(scene);
         stage.setFullScreen(true); // Método para poner la ventana en fullScreen
         stage.show();
